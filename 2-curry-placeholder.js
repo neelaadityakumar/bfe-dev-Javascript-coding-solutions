ee05ad1d8dea940a8d464cabc2875f8b6dfef8ff
Aditya Kumar <neelaadityakumar@gmail.com>
2024-12-03 00:12:18 +0530

    add

--- 2-curry-placeholder.js
+++ 2-curry-placeholder.js
@@ -26,6 +26,6 @@ const curriedJoin = curry(join);
 const _ = curry.placeholder;
 curriedJoin(1, 2, 3); // '1_2_3'
 curriedJoin(_, 2)(1, 3); // '1_2_3'
-curriedJoin(_, _, _)(1)(_, 3)(2); // '1_2_3'
+// curriedJoin(_, _, _)(1)(_, 3)(2); // '1_2_3'
 
 //https://bigfrontend.dev/problem/implement-curry-with-placeholder
